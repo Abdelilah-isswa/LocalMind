4438ee4b279a6f0d23e45fff3c8ae6e4c420e369
Abdelilah Isswa <tomhary66@gmail.com>
2026-01-29 11:26:44 +0100

    edit the rout

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,12 +17,28 @@ Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/', [QuestionController::class, 'home'])->name('home');
 
 
-Route::get('/dashboard', function () {
-    return 'Welcome ' . auth()->user()->name;
-})->middleware('auth');
+
 
 
 Route::middleware('auth')->group(function () {
+
+    Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create');
+    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
+    Route::post('/questions/{question}/comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
+
+    // Store new comment
+    Route::post('/questions/{question}/comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
+
+    // Edit form
+    Route::get('/comments/{comment}/edit', [\App\Http\Controllers\CommentController::class, 'edit'])->name('comments.edit');
+
+    // Update comment
+    Route::put('/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'update'])->name('comments.update');
+
+    // Delete comment
+    Route::delete('/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'destroy'])->name('comments.destroy');
+
+
 
     Route::get('/my-questions', [QuestionController::class, 'myQuestions'])
         ->name('questions.mine');
@@ -38,43 +54,15 @@ Route::middleware('auth')->group(function () {
 
     Route::delete('/questions/{question}', [QuestionController::class, 'destroy'])
         ->name('questions.destroy');
-});
-Route::middleware('auth')->group(function () {
-    Route::get('/questions/create', [QuestionController::class, 'create'])->name('questions.create');
-    Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
-});
 
-Route::middleware('auth')->group(function () {
-    Route::post('/questions/{question}/comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
-});
-
-
-Route::middleware('auth')->group(function () {
-    // Store new comment
-    Route::post('/questions/{question}/comments', [\App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
-
-    // Edit form
-    Route::get('/comments/{comment}/edit', [\App\Http\Controllers\CommentController::class, 'edit'])->name('comments.edit');
-
-    // Update comment
-    Route::put('/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'update'])->name('comments.update');
-
-    // Delete comment
-    Route::delete('/comments/{comment}', [\App\Http\Controllers\CommentController::class, 'destroy'])->name('comments.destroy');
-});
-Route::middleware('auth')->group(function () {
     Route::post('/questions/{question}/favorite', [FavoriteController::class, 'toggle'])
          ->name('questions.favorite');
-});
-
-
-
-
-
-Route::middleware('auth')->group(function () {
     // Favorite toggle
     Route::post('/questions/{question}/favorite', [FavoriteController::class, 'toggle'])->name('questions.favorite');
 
     // Favorite list page
     Route::get('/questions/favorites', [FavoriteController::class, 'index'])->name('questions.favorites');
+
+
+
 });
